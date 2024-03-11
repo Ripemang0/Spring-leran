ccbafeb7631bc9ec1eaca19be50129b398778472
Ripemango <letyoufly@email.cn>
2024-03-11 11:06:52 +0800

    Using JSON Matchers MockMVc Test List Beers Create JSON Using Jackon

--- src/test/java/ripemango/springframework/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/ripemango/springframework/spring6restmvc/controller/BeerControllerTest.java
@@ -1,5 +1,8 @@
 package ripemango.springframework.spring6restmvc.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -11,13 +14,14 @@ import ripemango.springframework.spring6restmvc.model.Beer;
 import ripemango.springframework.spring6restmvc.service.BeerService;
 import ripemango.springframework.spring6restmvc.service.BeerServiceImpl;
 
+import static org.hamcrest.core.Is.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import java.util.UUID;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import java.util.UUID;
 
 
 //@SpringBootTest
@@ -30,24 +34,65 @@ class BeerControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
 
     @MockBean
     BeerService beerService;
 
-    BeerServiceImpl beerServiceImpl = new BeerServiceImpl();
+    BeerServiceImpl beerServiceImpl;
 
+    @BeforeEach
+    void setUp(){
+        beerServiceImpl = new BeerServiceImpl();
+    }
+
+    @Test
+    void testCreateNewBeer() throws Exception {
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        objectMapper.findAndRegisterModules();
+
+        Beer beer = beerServiceImpl.listBeers().get(0);
+        beer.setVersion(null);
+        beer.setId(null);
+
+        given(beerService.saveNewBeer(any(Beer.class))).willReturn(beerServiceImpl.listBeers().get(1));
+
+        mockMvc.perform(post("/api/v1/beer")
+                .accept(MediaType.APPLICATION_JSON)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(beer)))
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("Location"));
+
+       // System.out.println(objectMapper.writeValueAsString(beer));
+    }
+
+
+    @Test
+    void testListBeers() throws Exception {
+        given(beerService.listBeers()).willReturn(beerServiceImpl.listBeers());
+
+        mockMvc.perform(get("/api/v1/beer")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect((content().contentType(MediaType.APPLICATION_JSON)))
+                .andExpect(jsonPath("$.length()",is(3)));
+    }
 
     @Test
     void getBeerById() throws Exception {
             Beer testBeer = beerServiceImpl.listBeers().get(0);
 
-            given(beerService.getBeerById(any(UUID.class))).willReturn(testBeer);
+            given(beerService.getBeerById(testBeer.getId())).willReturn(testBeer);
 
 
-            mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID())
+            mockMvc.perform(get("/api/v1/beer/" + testBeer.getId())
                     .accept(MediaType.APPLICATION_JSON))
                     .andExpect(status().isOk())
-                    .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+                    .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(jsonPath("$.id", is(testBeer.getId().toString())))
+                    .andExpect((jsonPath("$.beerName", is(testBeer.getBeerName()))));
 
 //        System.out.println(beerController.getBeerById(UUID.randomUUID()));
     }
